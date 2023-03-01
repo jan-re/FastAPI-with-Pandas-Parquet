213b598962b64721415c1faf64f6ab7db8bbf6b2
Jan Rehanek <rehanekjan@gmail.com>
2023-03-01 21:03:27 +0100

    progressing

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,22 +1,68 @@
+from __future__ import annotations
 from attrs import define
 import pandas as pd
 
 
 @define
+class DatasetStats():
+    name: str
+    columns: list[str]
+    length: int
+
+    def __init__(self, name, columns, length) -> None:
+        self.name = name
+        self.columns = columns
+        self.length = length
+
+
+@define
 class FlightData():
-    # aircraft: pd.DataFrame
-    # aircraft_models: pd.DataFrame
-    # airports: pd.DataFrame
-    # carriers: pd.DataFrame
-    # flights: pd.DataFrame
+    aircraft: pd.DataFrame
+    aircraft_models: pd.DataFrame
+    airports: pd.DataFrame
+    carriers: pd.DataFrame
+    flights: pd.DataFrame
+    handler: Handler
 
-    # def __init__(self) -> None:
-    #     self.aircraft = pd.read_parquet('./data/aircraft.parquet')
-    #     self.aircraft_models = pd.read_parquet('./data/aircraft_models.parquet')
-    #     self.airports = pd.read_parquet('./data/airports.parquet')
-    #     self.carriers = pd.read_parquet('./data/carriers.parquet')
-    #     self.flights = pd.read_parquet('./data/flights.parquet')
+    def __init__(self) -> None:
+        self.handler = Handler()
 
-    def return_data(self) -> pd.DataFrame:
-        return NotImplementedError
+    def load_data(self) -> None:
+        self.aircraft = pd.read_parquet('./app//data/aircraft.parquet')
+        self.aircraft_models = pd.read_parquet('./app/data/aircraft_models.parquet')
+        self.airports = pd.read_parquet('./app/data/airports.parquet')
+        self.carriers = pd.read_parquet('./app/data/carriers.parquet')
+        self.flights = pd.read_parquet('./app/data/flights.parquet')
 
+        print("Flight data loaded.")
+
+    def fetch_datasets(self) -> str:
+        return self.handler.handle_datasets(self)
+
+    def fetch_models(self) -> str:
+        return self.handler.handle_models(self.aircraft_models)
+
+    def fetch_active_aircraft(self, manufacturer: str | None, model: str | None) -> str:
+        return self.handler.handle_active_aircraft(self.aircraft, self.aircraft_models, manufacturer, model)
+
+
+@define
+class Handler():
+
+    def handle_datasets(self, flight_data: FlightData) -> str:
+
+        aircraft = DatasetStats("aircraft", list(flight_data.aircraft.columns), len(flight_data.aircraft.index))
+        aircraft_models = DatasetStats("aircraft_models", list(flight_data.aircraft_models.columns), len(flight_data.aircraft_models.index))
+        airports = DatasetStats("airports", list(flight_data.airports.columns), len(flight_data.airports.index))
+        carriers = DatasetStats("carriers", list(flight_data.carriers.columns), len(flight_data.carriers.index))
+        flights = DatasetStats("flights", list(flight_data.flights.columns), len(flight_data.flights.index))
+
+        # Continue from here
+
+        return NotImplemented
+    
+    def handle_models(self, aircraft_models: pd.DataFrame) -> str:
+        return NotImplemented
+
+    def handle_active_aircraft(self, aircraft: pd.DataFrame, aircraft_models: pd.DataFrame, manufacturer: str | None, model: str | None) -> str:
+        return NotImplemented
--- app/main.py
+++ app/main.py
@@ -1,18 +1,70 @@
 from fastapi import Depends, FastAPI
-from .routers import entities, reports
+from pydantic import BaseModel
 from .dependencies import FlightData
+
+
+class Query(BaseModel):
+    query: str
+
 
 app = FastAPI()
 
 flight_data: FlightData = FlightData()
 
 
-app.include_router(
-    entities.router,
-    dependencies=[Depends(flight_data.return_data())]
-)
+@app.on_event("startup")
+def startup_load():
+    flight_data.load_data()
 
-app.include_router(
-    reports.router,
-    dependencies=[Depends(flight_data.return_data())]
-)
+
+@app.get("/entities/datasets", tags=["entities"], status_code=200)
+async def read_datasets(data: str = Depends(flight_data.fetch_datasets)):
+    """
+    Clients can get information about the loaded data sets. For each of the FAA data file, service returns:
+    - Name of the data file
+    - List of columns
+    - Total number of rows
+    """
+    return NotImplemented
+
+
+@app.get("/entities/models", tags=["entities"], status_code=200)
+async def read_models(data: str = Depends(flight_data.fetch_models)):
+    """
+    Client can call with no parameters and that will return all known aircraft models, their manufacturer and number of seats.
+    All data needed for this endpoint is in the aircraft_models data file.
+    """
+    return NotImplemented
+
+
+@app.get("/entities/activeAircraft", tags=["entities"], status_code=200)
+async def read_active_aircraft(manufacturer: str | None = None, model: str | None = None, data: str = Depends(flight_data.fetch_active_aircraft)):
+    """
+    Client can call with two parameters:
+    - Aircraft manufacturer
+    - Aircraft model
+    Given this input, your service must return all active aircrafts of the selected model and manufacturer. For each found aircraft, the endpoint must return:
+    - Manufacturer
+    - Model,
+    - Number of seats
+    - Serial number
+    - Registrant name
+    - Registrant county
+    All data needed for this endpoint is in the aircraft and aircraft_models data files.
+    """
+    return NotImplemented
+
+
+#@app.get("/reports/modelsByCounty", tags=["reports"], status_code=200)
+#async def read_models_by_county(data: str = Depends(flight_data.fetch_datasets)):
+#    return NotImplemented
+#
+#
+#@app.get("/reports/modelsByCountyPivot", tags=["reports"], status_code=200)
+#async def read_models_by_county_pivot(data: str = Depends(flight_data.fe)):
+#    return NotImplemented
+#
+#
+#@app.post("/reports/customReport", tags=["reports"], status_code=200)
+#async def create_sql_report(query: Query):
+#    return NotImplemented
